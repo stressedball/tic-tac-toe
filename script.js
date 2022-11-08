c68b4714e3e9d5c21b83c8588ed152959717291f
stressedball <tserchesi@gmail.com>
2022-11-08 22:02:35 +0100

    make game choices. first screen for start game button. second screen for player 1 name and x-o choice. third screen for choice vs player or human if human fourth screen for player 2 name

--- script.js
+++ script.js
@@ -2,18 +2,15 @@
 
 window.addEventListener('keypress', inputManagement);
 window.addEventListener('click', clickManagement);
-window.addEventListener('load', onLoad);
+// window.addEventListener('load', onLoad);
 const gameboardContainer = document.querySelector('.gameboard');
 const playersContainer = document.querySelector('.container.players');
 const startGameButton = document.querySelector('button.start-game');
-const startGameButtonContainer = document.querySelector('div.button-start-container');
+// const startGameButtonContainer = document.querySelector('div.button-start-container');
 const gameContainerWidth = document.querySelector('.game').getBoundingClientRect().width;
 const gameContainerHeight = document.querySelector('.game').getBoundingClientRect().height;
-startGameButton.addEventListener('click', newGame);
-// const xoButtons = document.querySelectorAll('button');
-// for (let button of xoButtons) {
-//     button.addEventListener('click', X_O);
-// }
+// startGameButton.addEventListener('click', newGame);
+
 
 let gameboardArray = [];
 let player1, player2;
@@ -63,6 +60,11 @@ function clickManagement(event) {
         event.preventDefault();
         let playerName = document.getElementById('player-one-name').value;
     }
+
+    if (event.target.classList.contains('start-game')) {
+        newGame();
+        console.log('ok')
+    }
 }
 
 function inputManagement(inputKey) {
@@ -72,32 +74,34 @@ function inputManagement(inputKey) {
     }
 }
 
-function newGame() {
+function newGame(selector) {
+    // selector.classList.add('hide');
+    document.querySelector('.button-start-container').classList.add('hide-container');
+    document.querySelector('.game .button-start-container').classList.remove('button-start-container');
+
     // document.querySelector('div.hidden').classList.remove('hidden');
     // document.querySelector('.game > div').classList.add('game-start');
     // document.querySelector('.game').classList.add('game-start');
-
-    startGameButtonContainer.style.height = `${0.2 * gameContainerHeight}px`;
-    startGameButtonContainer.style.width = `${0.2 * gameContainerWidth}px`;
-    startGameButton.style.width = `${0.8 * startGameButtonContainer.getBoundingClientRect().width}px`;
-    startGameButton.style.height = `${0.8 * startGameButtonContainer.getBoundingClientRect().height}px`;
+    // startGameButtonContainer.style.height =`${0.2 * gameContainerHeight}px`;
+    // startGameButtonContainer.style.width = `${0.2 * gameContainerWidth}px`;
+    // startGameButton.style.width = `${0.8 * startGameButtonContainer.getBoundingClientRect().width}px`;
+    // startGameButton.style.height = `${0.8 * startGameButtonContainer.getBoundingClientRect().height}px`;
 
     // startGameButtonContainer.classList.add('game-start');
     // startGameButton.classList.remove('start-game');
     // startGameButton.classList.add('game-start');
-    startGameButton.textContent = 'Let\'s do this';
+    // startGameButton.textContent = 'Let\'s do this';
 }
 
-function onLoad() {
-    startGameButtonContainer.style.height = `${gameContainerHeight}px`;
-    startGameButtonContainer.style.width = `${gameContainerWidth}px`;
-    const startGameButtonContainerWidth = startGameButtonContainer.getBoundingClientRect().width;
-    const startGameButtonContainerHeight = startGameButtonContainer.getBoundingClientRect().height;
-    startGameButton.style.width = `${0.8 * startGameButtonContainerWidth}px`;
-    startGameButton.style.height = `${0.8 * startGameButtonContainerHeight}px`;
-    startGameButton.style.transform = `translate(${(startGameButtonContainerWidth - (0.8 * startGameButtonContainerWidth)) / 2}px, ${(startGameButtonContainerHeight - (0.8 * startGameButtonContainerHeight)) / 2}px)`;
-    // console.log(startGameButtonHeight, startGameButtonWidth)
-}
+// function onLoad() {
+//     startGameButtonContainer.style.height = `${gameContainerHeight}px`;
+//     startGameButtonContainer.style.width = `${gameContainerWidth}px`;
+//     const startGameButtonContainerWidth = startGameButtonContainer.getBoundingClientRect().width;
+//     const startGameButtonContainerHeight = startGameButtonContainer.getBoundingClientRect().height;
+//     startGameButton.style.width = `${0.5 * startGameButtonContainerWidth}px`;
+//     startGameButton.style.height = `${0.5 * startGameButtonContainerHeight}px`;
+//     startGameButton.style.transform = `translate(${(startGameButtonContainerWidth - (0.5 * startGameButtonContainerWidth)) / 2}px, ${(startGameButtonContainerHeight - (0.5 * startGameButtonContainerHeight)) / 2}px)`;
+// }
 
 function styleButtonStart() {
 
